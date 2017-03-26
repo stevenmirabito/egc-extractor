4aae9438783900faab5ac953884d7d78335ec96b
Steven Mirabito <steven@stevenmirabito.com>
2017-03-26 14:38:59 -0700

    Initial commit

---
+++ config.sample.py
@@ -0,0 +1,10 @@
+CHROMEDRIVER_PATH = "/usr/local/bin/chromedriver"
+
+IMAP_HOST = "imap.gmail.com"
+IMAP_PORT = 993
+IMAP_SSL = True
+IMAP_USERNAME = "you@gmail.com"
+IMAP_PASSWORD = ""
+
+FOLDER = "INBOX"
+FROM_EMAIL = "DoNotReply.Staples@blackhawk-net.com"
